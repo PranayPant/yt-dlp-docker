c87fb7d19391e0c4978ff051297ada5b67090f0e
PranayPant <ppantg1@gmail.com>
2024-11-13 11:46:40 -0600

    feat: initial working code

--- infra/app.py
+++ infra/app.py
@@ -11,7 +11,7 @@ DEFAULT_OPTS =  {
     }
 }
 
-def download_subs(url: str, filename: str | None = None, yt_opts=None):
+def download_subs(url, filename=None, yt_opts=None):
 
     yt_opts = yt_opts or {}
     opts = {**DEFAULT_OPTS, **yt_opts}
@@ -30,5 +30,22 @@ def download_subs(url: str, filename: str | None = None, yt_opts=None):
 
     return content
 
-def lambda_handler(event, context):
-    download_subs()
+def lambda_handler(event, lambda_context):
+    try:
+        id = event['queryStringParameters']['videoId']
+        subtitle_text = download_subs(f'https://www.youtube.com/watch?v={id}')
+        return {
+            "statusCode": 200,
+            "headers": {
+                "Content-Type": "plain/text"
+            },
+            "body": subtitle_text
+        }
+    except Exception:
+        return {
+            "statusCode": 500,
+            "headers": {
+                "Content-Type": "plain/text"
+            },
+            "body": 'Internal Server Error'
+        }
